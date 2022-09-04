2500712898a681bb5163bea30b803132e7a582d0
Adi Vrskic <adivrskic123@gmail.com>
2022-09-04 12:06:12 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import PostList from "./PostList";
+import "../style/App.css";
+
+function App() {
+  return (
+    <div className="ui container">
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
